5204307de695b7a1d0db388e7079a3706b88384d
Robert J Maneno <94848927+robertjmaneno@users.noreply.github.com>
2023-06-03 13:23:15 +0200

    adding the nav bar widget

--- toyota_shop/lib/screeens/mainScreen.dart
+++ toyota_shop/lib/screeens/mainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toyota_shop/views/shared/appStyle.dart';
+import 'package:toyota_shop/views/shared/bottom_nav_bar.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -7,6 +8,32 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFE2E2E2),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.green[600],
+              borderRadius: BorderRadius.all(
+                Radius.circular(16),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                BottomNavBar(),
+                BottomNavBar(),
+                BottomNavBar(),
+                BottomNavBar(),
+                BottomNavBar()
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Center(
         child: Text(
           'Main Screen',
