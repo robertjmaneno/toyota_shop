83718d874c314861d9a48cb8d6eb4f9528bb5e01
Robert J Maneno <94848927+robertjmaneno@users.noreply.github.com>
2023-06-03 11:41:17 +0200

    removing the debugger

--- toyota_shop/lib/main.dart
+++ toyota_shop/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Toyota Shop App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
