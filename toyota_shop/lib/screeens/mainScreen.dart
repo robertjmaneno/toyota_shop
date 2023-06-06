5b074be7f0f7a0ab167c5db1c108b3b2b3cca274
Robert J Maneno <94848927+robertjmaneno@users.noreply.github.com>
2023-06-06 10:10:18 +0200

    adding the page list in my body property

--- toyota_shop/lib/screeens/mainScreen.dart
+++ toyota_shop/lib/screeens/mainScreen.dart
@@ -3,12 +3,16 @@ import 'package:toyota_shop/views/shared/appStyle.dart';
 import 'package:toyota_shop/views/shared/bottom_nav_bar.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+  MainScreen({super.key});
+
+  List<Widget> pageList = [];
 
   @override
   Widget build(BuildContext context) {
+    int pageIndex = 0;
     return Scaffold(
       backgroundColor: const Color(0xFFE2E2E2),
+      body: pageList[pageIndex],
       bottomNavigationBar: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(10),
@@ -24,20 +28,29 @@ class MainScreen extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                BottomNavBar(),
-                BottomNavBar(),
-                BottomNavBar(),
-                BottomNavBar(),
-                BottomNavBar()
+                BottomNavBar(
+                  onTap: () {},
+                  icon: Icons.home,
+                ),
+                BottomNavBar(
+                  onTap: () {},
+                  icon: Icons.search,
+                ),
+                BottomNavBar(
+                  onTap: () {},
+                  icon: Icons.add,
+                ),
+                BottomNavBar(
+                  onTap: () {},
+                  icon: Icons.add_shopping_cart_sharp,
+                ),
+                BottomNavBar(
+                  onTap: () {},
+                  icon: Icons.person,
+                ),
               ],
             ),
           ),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'Main Screen',
-          style: appStyle(20, Colors.black, FontWeight.bold),
         ),
       ),
     );
